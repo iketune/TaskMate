ff82b94416b28be95c3846f2565812241780faa4
池田 大輝 <iketune1149@gmail.com>
2025-07-17 19:56:47 +0900

    rename project

--- src/main/java/com/example/taskmate/TaskMateApplication.java
+++ src/main/java/com/example/taskmate/TaskMateApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskMate04Application {
+public class TaskMateApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TaskMate04Application.class, args);
+		SpringApplication.run(TaskMateApplication.class, args);
 	}
 
 }
